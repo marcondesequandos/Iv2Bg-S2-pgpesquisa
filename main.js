dcb3fd2d2005c8382aea6b3a825c9cfbc6ad1b77
Felipe Marcondes <felipemgcarvalho@gmail.com>
2021-01-19 10:41:16 -0300

    Iv2 Beginners - Projeto semana 2 final

---
+++ main.js
@@ -0,0 +1,11 @@
+var estilopg = document.getElementById('pgstyle')
+
+function changePgHREF(){
+    if(estilopg.href == "https://code.getmdl.io/1.3.0/material.light_green-green.min.css"){
+        estilopg.href = "https://code.getmdl.io/1.3.0/material.orange-deep_orange.min.css"
+    } else{
+        estilopg.href = 'https://code.getmdl.io/1.3.0/material.light_green-green.min.css'
+    }
+}
+
+
